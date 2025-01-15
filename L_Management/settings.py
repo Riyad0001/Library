f8c308496dd1a88a3d74c7432fdb5833433a22f5
Riyad0001 <rieadhasan499@gmail.com>
2025-01-16 03:03:55 +0600

    deploy first

--- L_Management/settings.py
+++ L_Management/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =env('SECRET_KEY')
-CSRF_TRUSTED_ORIGINS = ['https://library-0c15.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://library-0c15.onrender.com','httpps://*.127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
